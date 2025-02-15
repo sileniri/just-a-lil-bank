9ad95d62310a0cf7a23ba6141bf239ecc64f7206
sileniri <sigurrinnan@gmail.com>
2025-02-15 18:10:58 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function openMenu() {
+    document.querySelector('#openMenu').classList.remove('blinkMe')
+    document.querySelector('.websiteContainer').classList.toggle('active');
+    document.querySelector('#openMenu').classList.toggle('active');
+    document.querySelector('#openIcon').classList.toggle('active');
+    document.querySelector('#closeIcon').classList.toggle('active');
+}
+
+function openSubpage(input) {
+    const subpages = document.querySelectorAll('.subpage');
+    for (const subpage of subpages) {
+        subpage.classList.add('hidden')
+    }
+    document.querySelector('#'+input).classList.remove('hidden');
+}
+
+function setTheme(theme) {
+    document.querySelector('#pageStyle').href = '/main/' + theme + '.css';
+    document.querySelector('.dropDownBtn').innerHTML = theme + ' Theme';
+}
